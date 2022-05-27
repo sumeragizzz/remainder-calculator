37dc5f60aea4bf46ad6781c086ad486371ea67a7
sumeragizzz <developer@sumeragizzz.dev>
2022-05-27 15:47:01 +0900

    不要なコードを削除

--- js/app.js
+++ js/app.js
@@ -1,16 +1,3 @@
-const Counter = {
-    data() {
-      return {
-        counter: 0
-      }
-    },
-    mounted() {
-        setInterval(() => {
-          this.counter++
-        }, 1000)
-    }
-}
-
 Vue.createApp({
     data() {
         return {
